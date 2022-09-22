f1c43d24a2d859d01401716a0275f9187ca40cdf
pehtoorismies <pehtorismies@gmail.com>
2022-09-22 13:38:24 +0300

    refactor: use common function to create Config

--- stacks/config-stack.ts
+++ stacks/config-stack.ts
@@ -1,4 +1,4 @@
-import { Config, StackContext } from '@serverless-stack/resources'
+import { Config, Stack, StackContext } from '@serverless-stack/resources'
 
 const getEnvironmentVariable = (name: string): string => {
   const value = process.env[name]
@@ -8,26 +8,19 @@ const getEnvironmentVariable = (name: string): string => {
   return value
 }
 
+const createConfigFromEnvironment = (stack: Stack, name: string) => {
+  const config = new Config.Parameter(stack, name, {
+    value: getEnvironmentVariable(name),
+  })
+  return config
+}
+
 export const ConfigStack = ({ stack }: StackContext) => {
-  const AUTH_DOMAIN = new Config.Parameter(stack, 'AUTH_DOMAIN', {
-    value: getEnvironmentVariable('AUTH_DOMAIN'),
-  })
-
-  const AUTH_CLIENT_ID = new Config.Parameter(stack, 'AUTH_CLIENT_ID', {
-    value: getEnvironmentVariable('AUTH_CLIENT_ID'),
-  })
-  const JWT_AUDIENCE = new Config.Parameter(stack, 'JWT_AUDIENCE', {
-    value: getEnvironmentVariable('JWT_AUDIENCE'),
-  })
-
-  const AUTH_CLIENT_SECRET = new Config.Secret(stack, 'AUTH_CLIENT_SECRET')
-  const REGISTER_SECRET = new Config.Secret(stack, 'REGISTER_SECRET')
-
   return {
-    AUTH_CLIENT_ID,
-    AUTH_CLIENT_SECRET,
-    AUTH_DOMAIN,
-    JWT_AUDIENCE,
-    REGISTER_SECRET,
+    AUTH_CLIENT_ID: createConfigFromEnvironment(stack, 'AUTH_CLIENT_ID'),
+    AUTH_CLIENT_SECRET: new Config.Secret(stack, 'AUTH_CLIENT_SECRET'),
+    AUTH_DOMAIN: createConfigFromEnvironment(stack, 'AUTH_DOMAIN'),
+    JWT_AUDIENCE: createConfigFromEnvironment(stack, 'JWT_AUDIENCE'),
+    REGISTER_SECRET: new Config.Secret(stack, 'REGISTER_SECRET'),
   }
 }
