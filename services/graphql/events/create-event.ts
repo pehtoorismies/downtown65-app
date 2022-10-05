f86e84753a479b2911afc93b2b2de581d84ba6d4
pehtoorismies <pehtorismies@gmail.com>
2022-10-05 13:00:47 +0300

    feat: separate api and domain

--- services/graphql/events/create-event.ts
+++ services/graphql/events/create-event.ts
@@ -1,47 +1,24 @@
 import type { AppSyncResolverHandler } from 'aws-lambda'
-import formatISO from 'date-fns/formatISO'
-import { v4 as uuidv4 } from 'uuid'
 import type { Event as Dt65Event, MutationCreateEventArgs } from '../../appsync'
-import { getTable } from '../../dynamo/table'
-import { getPrimaryKey } from './support/event-primary-key'
-import type { EventType } from './support/event-type'
+import * as Event from '../../core/event'
+import { EVENT_TYPES, isEventType } from '../../core/event-type'
 
 export const createEvent: AppSyncResolverHandler<
   MutationCreateEventArgs,
   Dt65Event
 > = async (event) => {
-  const Table = getTable()
+  const { event: creatableEvent } = event.arguments
 
-  const { title, dateStart, type, subtitle, race } = event.arguments.event
-
-  const eventId = uuidv4()
-  const startDate = formatISO(new Date(dateStart))
-
-  await Table.Dt65Event.put(
-    {
-      // add keys
-      ...getPrimaryKey(eventId),
-      GSI1PK: `EVENT#FUTURE`,
-      GSI1SK: `DATE#${startDate}#${eventId.slice(0, 8)}`,
-      // add props
-      createdBy: 'add later',
-      dateStart: startDate,
-      id: eventId,
-      title,
-      subtitle,
-      race: race ?? false,
-      participants: {},
-      type,
-    },
-    { returnValues: 'none' }
-  )
-
-  return {
-    id: eventId,
-    title,
-    subtitle: subtitle ?? undefined,
-    race: race ?? false,
-    type: type as EventType,
-    dateStart: startDate,
+  if (!isEventType(creatableEvent.type)) {
+    throw new Error(
+      `Schema mismatch. 'type' is not within ${EVENT_TYPES.join(',')} `
+    )
   }
+
+  return Event.create({
+    ...creatableEvent,
+    race: creatableEvent.race ?? false,
+    subtitle: creatableEvent.subtitle ?? undefined,
+    type: creatableEvent.type,
+  })
 }
--- services/graphql/support/event.ts
+++
@@ -1,9 +0,0 @@
-import type { EventType } from '../events/support/event-type'
-
-export interface Event {
-  id: string
-  title: string
-  type: EventType
-  subtitle?: string
-  race: boolean
-}
