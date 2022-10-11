696d5d7b1662118bfcbb35f0a3656881332ada6c
pehtoorismies <pehtorismies@gmail.com>
2022-10-11 10:37:32 +0300

    feat: add remix

---
+++ packages/frontend/app/routes/auth/forgot-password.tsx
@@ -0,0 +1,9 @@
+const forgotPassword = () => {
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+      <h1>Forgot password</h1>
+    </div>
+  )
+}
+
+export default forgotPassword
