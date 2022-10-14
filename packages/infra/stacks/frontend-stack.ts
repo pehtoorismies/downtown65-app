5a2f9185589960ab9affb92779f546fa46ab6109
pehtoorismies <pehtorismies@gmail.com>
2022-10-14 09:30:12 +0300

    infra: add api related data as environment for frontend

--- packages/infra/stacks/frontend-stack.ts
+++ packages/infra/stacks/frontend-stack.ts
@@ -1,12 +1,18 @@
 import type { StackContext } from '@serverless-stack/resources'
-import { RemixSite } from '@serverless-stack/resources'
+import { RemixSite, use } from '@serverless-stack/resources'
+import { GraphqlStack } from './graphql-stack'
 
 export const FrontendStack = ({ stack }: StackContext) => {
   // ... existing constructs
+  const api = use(GraphqlStack)
 
   // Create the Remix site
   const site = new RemixSite(stack, 'Downtown65Site', {
     path: 'packages/frontend',
+    environment: {
+      API_URL: api.ApiUrl,
+      API_KEY: api.ApiKey,
+    },
   })
 
   // Add the site's URL to stack output
