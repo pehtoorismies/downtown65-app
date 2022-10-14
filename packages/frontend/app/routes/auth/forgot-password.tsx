1defbc372766d5f18bc4faf176fb565235eff119
pehtoorismies <pehtorismies@gmail.com>
2022-10-14 18:21:46 +0300

    feat: add form skeletons

--- packages/frontend/app/routes/auth/forgot-password.tsx
+++ packages/frontend/app/routes/auth/forgot-password.tsx
@@ -1,9 +1,83 @@
-const forgotPassword = () => {
+import {
+  Anchor,
+  Box,
+  Button,
+  Center,
+  Container,
+  Group,
+  Paper,
+  Text,
+  TextInput,
+  Title,
+  createStyles,
+} from '@mantine/core'
+import { useNavigate } from '@remix-run/react'
+import { IconArrowLeft } from '@tabler/icons'
+
+const styles = createStyles((theme) => ({
+  title: {
+    fontSize: 26,
+    fontWeight: 900,
+  },
+
+  controls: {
+    [theme.fn.smallerThan('xs')]: {
+      flexDirection: 'column-reverse',
+    },
+  },
+
+  control: {
+    [theme.fn.smallerThan('xs')]: {
+      width: '100%',
+      textAlign: 'center',
+    },
+  },
+}))
+
+const ForgotPassword = () => {
+  const navigation = useNavigate()
+
+  const { classes } = styles()
+
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Forgot password</h1>
-    </div>
+    <Container size={460} my={30}>
+      <Title
+        align="center"
+        sx={(theme) => ({
+          fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+          fontWeight: 900,
+        })}
+      >
+        Salasana unohtunut
+      </Title>
+      <Text color="dimmed" size="sm" align="center">
+        Syötä sähköpostiosoitteesi saadaksesi sähköpostiisi ohjeet salasanan
+        resetoimiseksi.
+      </Text>
+
+      <Paper withBorder shadow="md" p={30} radius="md" mt="xl">
+        <TextInput
+          label="Sähköpostiosoitteesi"
+          placeholder="me@downtown65.com"
+          required
+        />
+        <Group position="apart" mt="lg" className={classes.controls}>
+          <Anchor
+            color="dimmed"
+            size="sm"
+            className={classes.control}
+            onClick={() => navigation('/auth/login')}
+          >
+            <Center inline>
+              <IconArrowLeft size={12} stroke={1.5} />
+              <Box ml={5}>Takaisin kirjautumiseen</Box>
+            </Center>
+          </Anchor>
+          <Button className={classes.control}>Lähetä ohjeet</Button>
+        </Group>
+      </Paper>
+    </Container>
   )
 }
 
-export default forgotPassword
+export default ForgotPassword
--- packages/frontend/app/routes/auth/signup.tsx
+++ packages/frontend/app/routes/auth/signup.tsx
@@ -1,9 +1,75 @@
-const signup = () => {
+import {
+  Anchor,
+  Button,
+  Container,
+  Group,
+  Paper,
+  PasswordInput,
+  Text,
+  TextInput,
+  Title,
+} from '@mantine/core'
+import { useNavigate } from '@remix-run/react'
+
+const Signup = () => {
+  const navigation = useNavigate()
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Signup</h1>
-    </div>
+    <Container size={420} my={40}>
+      <Title
+        align="center"
+        sx={(theme) => ({
+          fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+          fontWeight: 900,
+        })}
+      >
+        Rekisteröidy
+      </Title>
+      <Text size="sm" align="center" mt={5}>
+        Rekiteröitymiseen tarvitset seuran jäsenyyden ja liittymistunnuksen.
+      </Text>
+
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <TextInput
+          label="Sähköposti"
+          placeholder="me@downtown65.com"
+          required
+        />
+        <PasswordInput
+          label="Salasana"
+          placeholder="Salasanasi"
+          required
+          mt="md"
+        />
+        <TextInput
+          label="Nimi"
+          placeholder="Etunimi Sukunimi"
+          required
+          mt="md"
+        />
+        <TextInput
+          label="Lempinimi / nickname"
+          placeholder="setämies72"
+          required
+          mt="md"
+        />
+        <PasswordInput
+          label="Rekisteröintitunnus (kysy seuralta)"
+          placeholder="supersecret"
+          required
+          mt="md"
+        />
+
+        <Group position="right" mt="md">
+          <Anchor onClick={() => navigation('/auth/login')} size="sm">
+            Kirjautumiseen
+          </Anchor>
+        </Group>
+        <Button fullWidth mt="xl">
+          Rekisteröidy
+        </Button>
+      </Paper>
+    </Container>
   )
 }
 
-export default signup
+export default Signup
