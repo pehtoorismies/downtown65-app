52811c9570829a2261465e06fc35399f3505b0e9
pehtoorismies <pehtorismies@gmail.com>
2022-10-19 12:14:09 +0300

    feat: send forgot-password instructions via auth0

--- packages/services/src/graphql/auth/forgot-password.ts
+++ packages/services/src/graphql/auth/forgot-password.ts
@@ -1,12 +1,20 @@
 import type { AppSyncResolverHandler } from 'aws-lambda'
 import type { MutationForgotPasswordArgs } from '~/appsync.gen'
+import { getClient } from '~/support/auth0'
+
+const auth0 = getClient()
 
 export const forgotPassword: AppSyncResolverHandler<
   MutationForgotPasswordArgs,
   boolean
 > = async (event) => {
-  if (!event.arguments.email) {
-    console.error('TODO')
+  try {
+    await auth0.requestChangePasswordEmail({
+      email: event.arguments.email,
+      connection: 'Username-Password-Authentication',
+    })
+  } catch (error) {
+    console.error(error)
   }
 
   return true
