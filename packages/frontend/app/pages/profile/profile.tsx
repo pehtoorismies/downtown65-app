19c79f475478f55765716a91056d51731e48beb3
pehtoorismies <pehtorismies@gmail.com>
2022-11-02 19:17:29 +0200

    feat: fix profile layout

--- packages/frontend/app/pages/profile/profile.tsx
+++ packages/frontend/app/pages/profile/profile.tsx
@@ -13,6 +13,15 @@ import { IconLogout, IconX } from '@tabler/icons'
 import { useState } from 'react'
 import type { LoaderData } from './loader'
 
+const switchStyles = {
+  label: {
+    paddingLeft: 0,
+  },
+  labelWrapper: {
+    marginLeft: 10,
+  },
+}
+
 interface UserPreferences {
   weekly: boolean
   eventCreated: boolean
@@ -34,7 +43,7 @@ export const Profile = () => {
   }
 
   return (
-    <Paper radius="md" withBorder p="lg" m="md">
+    <Paper radius="md" withBorder p="lg" m="lg">
       <Avatar
         src="https://s.gravatar.com/avatar/176eb6f65cfff68dbcdde334af6e90da?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fpe.png"
         size={120}
@@ -48,141 +57,79 @@ export const Profile = () => {
         {name}
       </Text>
       <Divider my="sm" />
+      <Text size="lg" weight={500} mt="md">
+        Sähköpostiasetukset
+      </Text>
       <Group position="center">
-        <SimpleGrid cols={1} ml="lg">
-          <Text size="lg" weight={500} mt="md">
-            Sähköpostiasetukset:
-          </Text>
+        <Form method="post">
+          <Switch
+            styles={switchStyles}
+            name="eventCreated"
+            label="Lähetä sähköposti, kun uusi tapahtuma luodaan."
+            checked={emailSettings.eventCreated}
+            onChange={(event) => {
+              setEmailSettings({
+                weekly: emailSettings.weekly,
+                eventCreated: event.currentTarget.checked,
+              })
+            }}
+            onLabel="ON"
+            offLabel="OFF"
+            size="md"
+          />
+          <Switch
+            name="weekly"
+            styles={switchStyles}
+            label="Lähetä viikon tapahtumat sähköpostitse."
+            onLabel="ON"
+            offLabel="OFF"
+            checked={emailSettings.weekly}
+            onChange={(event) => {
+              setEmailSettings({
+                weekly: event.currentTarget.checked,
+                eventCreated: emailSettings.eventCreated,
+              })
+            }}
+            size="md"
+          />
 
-          <Form method="post">
-            <Switch
-              name="eventCreated"
-              label="Lähetä sähköposti, kun uusi tapahtuma luodaan."
-              checked={emailSettings.eventCreated}
-              onChange={(event) => {
-                setEmailSettings({
-                  weekly: emailSettings.weekly,
-                  eventCreated: event.currentTarget.checked,
-                })
-              }}
-              onLabel="ON"
-              offLabel="OFF"
-              size="lg"
-            />
-            <Switch
-              my="md"
-              name="weekly"
-              label="Lähetä viikon tapahtumat sähköpostitse."
-              onLabel="ON"
-              offLabel="OFF"
-              checked={emailSettings.weekly}
-              onChange={(event) => {
-                setEmailSettings({
-                  weekly: event.currentTarget.checked,
-                  eventCreated: emailSettings.eventCreated,
-                })
-              }}
-              size="lg"
-            />
-
-            <Group position="apart">
-              <Button
-                type="submit"
-                loading={
-                  transition.state === 'submitting' ||
-                  transition.state === 'loading'
-                }
-                disabled={
-                  preferences.subscribeWeeklyEmail === emailSettings.weekly &&
-                  preferences.subscribeEventCreationEmail ===
-                    emailSettings.eventCreated
-                }
-                leftIcon={<IconLogout size={18} />}
-                styles={(theme) => ({
-                  root: {
-                    backgroundColor: theme.colors.dtPink[6],
-                    border: 0,
-                    height: 42,
-                    paddingLeft: 20,
-                    paddingRight: 20,
-
-                    '&:hover': {
-                      backgroundColor: theme.fn.darken(
-                        theme.colors.dtPink[6],
-                        0.05
-                      ),
-                    },
-                  },
-
-                  leftIcon: {
-                    marginRight: 5,
-                  },
-                })}
-              >
-                Tallenna asetukset
-              </Button>
-              <Button
-                ml="md"
-                leftIcon={<IconX size={18} />}
-                styles={(theme) => ({
-                  root: {
-                    backgroundColor: theme.colors.blue[6],
-                    border: 0,
-                    height: 42,
-                    paddingLeft: 20,
-                    paddingRight: 20,
-
-                    '&:hover': {
-                      backgroundColor: theme.fn.darken(
-                        theme.colors.blue[6],
-                        0.05
-                      ),
-                    },
-                  },
-
-                  leftIcon: {
-                    marginRight: 5,
-                  },
-                })}
-                onClick={resetEmailPreferences}
-              >
-                Palauta muutokset
-              </Button>
-            </Group>
-          </Form>
-        </SimpleGrid>
-      </Group>
-      <Divider my="sm" />
-      <Group position="center">
-        <SimpleGrid cols={1} ml="lg">
-          <Text size="lg" weight={500} mt="md">
-            Uloskirjautuminen:
-          </Text>
-          <Form action="/logout" method="post">
+          <SimpleGrid
+            mt="sm"
+            cols={2}
+            breakpoints={[{ maxWidth: 600, cols: 1, spacing: 'sm' }]}
+          >
             <Button
               type="submit"
+              loading={
+                transition.state === 'submitting' ||
+                transition.state === 'loading'
+              }
+              disabled={
+                preferences.subscribeWeeklyEmail === emailSettings.weekly &&
+                preferences.subscribeEventCreationEmail ===
+                  emailSettings.eventCreated
+              }
               leftIcon={<IconLogout size={18} />}
-              styles={(theme) => ({
-                root: {
-                  backgroundColor: theme.colors.blue[6],
-                  border: 0,
-                  height: 42,
-                  paddingLeft: 20,
-                  paddingRight: 20,
-
-                  '&:hover': {
-                    backgroundColor: theme.fn.darken(
-                      theme.colors.blue[6],
-                      0.05
-                    ),
-                  },
-                },
-
-                leftIcon: {
-                  marginRight: 5,
-                },
-              })}
             >
+              Tallenna asetukset
+            </Button>
+            <Button
+              leftIcon={<IconX size={18} />}
+              onClick={resetEmailPreferences}
+            >
+              Palauta muutokset
+            </Button>
+          </SimpleGrid>
+        </Form>
+      </Group>
+      <Divider my="sm" />
+      <Text size="lg" weight={500} mt="md">
+        Uloskirjautuminen
+      </Text>
+      <Group position="center">
+        <SimpleGrid cols={1} ml="lg">
+          <Form action="/logout" method="post">
+            <Button type="submit" leftIcon={<IconLogout size={18} />}>
               Kirjaudu ulos
             </Button>
           </Form>
