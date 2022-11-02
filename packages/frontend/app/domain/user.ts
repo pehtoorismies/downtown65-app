b3d6575dc8b6a173623c78cad8a2d7445e1a2696
pehtoorismies <pehtorismies@gmail.com>
2022-11-02 12:02:07 +0200

    feat: streamline User object to include transformation

--- packages/frontend/app/domain/user.ts
+++ packages/frontend/app/domain/user.ts
@@ -1,13 +1,16 @@
 import { z } from 'zod'
 
-export const User = z.object({
-  nickname: z.string(),
-  sub: z.string(),
-  picture: z.string(),
-})
+export const User = z
+  .object({
+    nickname: z.string(),
+    sub: z.string(),
+    picture: z.string(),
+  })
+  .transform(({ sub, ...rest }) => {
+    return {
+      ...rest,
+      id: sub,
+    }
+  })
 
-export type User = {
-  nickname: string
-  id: string
-  picture: string
-}
+export type User = z.infer<typeof User>
