4dcf154b0bf168da50d64f82aeb007dd323e5bb7
pehtoorismies <pehtorismies@gmail.com>
2022-11-10 13:54:02 +0200

    feat: format date

--- packages/frontend/app/pages/events/events/loader.ts
+++ packages/frontend/app/pages/events/events/loader.ts
@@ -2,6 +2,7 @@ import type { LoaderFunction } from '@remix-run/node'
 import { json, redirect } from '@remix-run/node'
 import { getGqlSdk } from '~/gql/get-gql-client.server'
 import type { EventLoaderData } from '~/pages/events/event-loader-data'
+import { formatDate } from '~/pages/events/format-date'
 import { validateSessionUser } from '~/session.server'
 
 export interface LoaderData {
@@ -25,9 +26,10 @@ export const loader: LoaderFunction = async ({ request }) => {
   const mapped = events.map((event) => {
     return {
       ...event,
-      me: result.user,
-      isRace: event.race,
+      dateStart: formatDate(event.dateStart),
       description: event.description ?? '',
+      isRace: event.race,
+      me: result.user,
     }
   })
 
