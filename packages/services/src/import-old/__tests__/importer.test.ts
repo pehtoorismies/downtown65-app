ac93aebd5ff2d338c60cd78c4ff1485ac5945a63
pehtoorismies <pehtorismies@gmail.com>
2022-11-22 20:26:41 +0200

    test: remove test

--- packages/services/src/import-old/__tests__/importer.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from 'vitest'
-import { importEvents } from '~/core/event'
-
-test.skip('Import stuff', async () => {
-  const { id } = await importEvents()
-  expect(id).toBeDefined()
-})
