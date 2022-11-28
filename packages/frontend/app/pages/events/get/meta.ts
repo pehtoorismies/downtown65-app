9d38ba80e6e6f3931403c5910996bc9ad25cc0d1
pehtoorismies <pehtorismies@gmail.com>
2022-11-28 20:08:04 +0200

    feat: improve share link

--- packages/frontend/app/pages/events/get/meta.ts
+++ packages/frontend/app/pages/events/get/meta.ts
@@ -4,14 +4,14 @@ import { mapToData } from '~/util/event-type'
 
 export const meta: MetaFunction<typeof loader> = ({ data, location }) => {
   const { eventItem, origin } = data
-
+  const typeData = mapToData(eventItem.type)
   return {
     title: eventItem.title,
     'og:type': 'website',
     'og:url': `${origin}${location.pathname}`,
-    'og:title': `Event: ${eventItem.title}`,
+    'og:title': `${eventItem.title}`,
     'og:description': `${eventItem.dateStart} - ${eventItem.subtitle}`,
-    'og:image': `${origin}${mapToData(eventItem.type).imageUrl}`,
+    'og:image': `${origin}${typeData.imageUrl}`,
     'og:image:type': 'image/jpg',
   }
 }
