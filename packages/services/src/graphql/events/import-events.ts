442fcde00f6eafc4d230ea81e911eed3a025a8c5
pehtoorismies <pehtorismies@gmail.com>
2022-12-15 11:51:27 +0200

    feat: add picture to imported events

--- packages/services/src/graphql/events/import-events.ts
+++ packages/services/src/graphql/events/import-events.ts
@@ -5,16 +5,24 @@ import * as Event from '~/core/event'
 import { getImportedEvents } from '~/import-old/get-imported-events'
 
 const importUrl =
-  'https://storage.googleapis.com/downtown65/events/prod-events.json'
+  'https://storage.googleapis.com/downtown65/events/prod-events.txt'
 
 export const importEvents: AppSyncResolverHandler<
   MutationImportEventsArgs,
   string
-> = async () => {
-  const response = await fetch(importUrl)
-  const body = await response.text()
-  const oldEvents = getImportedEvents(body)
+> = async (event) => {
+  try {
+    const { start, end } = event.arguments
+    const response = await fetch(importUrl)
 
-  await Event.importEvents(oldEvents)
-  return 'success'
+    const body = await response.text()
+    // console.log(body.slice(0, 199))
+    const oldEvents = getImportedEvents(body, start, end)
+
+    await Event.importEvents(oldEvents)
+    return 'success'
+  } catch (error) {
+    console.error(error)
+    return 'unsuccessful'
+  }
 }
