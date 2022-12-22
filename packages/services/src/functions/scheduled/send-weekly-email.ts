191b0f128812b3d50d75d53c7f311ca005278bea
pehtoorismies <pehtorismies@gmail.com>
2022-12-22 10:33:08 +0200

    fix: comment out weeklywq

--- packages/services/src/functions/scheduled/send-weekly-email.ts
+++ packages/services/src/functions/scheduled/send-weekly-email.ts
@@ -1,5 +1,5 @@
-import { getWeeklyEmail } from '~/email/get-weekly-email'
-import { sendEmail } from '~/email/send-email'
+// import { getWeeklyEmail } from '~/email/get-weekly-email'
+// import { sendEmail } from '~/email/send-email'
 // import { getAuth0Management } from '../../support/auth0'
 
 // const management = await getAuth0Management()
@@ -20,34 +20,31 @@ import { sendEmail } from '~/email/send-email'
 // }
 
 export const main = async () => {
-  const { plain, html } = getWeeklyEmail({
-    preferencesUrl: 'http/sdfsdsfd',
-    events: [
-      {
-        subtitle: 'asdads',
-        title: 'asdads',
-        date: '11.11.2022',
-        weekDay: '12',
-        participantCount: 12,
-        url: 'asd',
-        type: 'kokoasd',
-        typeHeader: 'Type header tässä',
-      },
-    ],
-  })
-
-  const result = await sendEmail({
-    subject: 'Viikon tapahtumat',
-    body: {
-      html,
-      text: plain,
-    },
-    to: 'pehtoorismies@gmail.com',
-    from: `Kyttaki <kyttaki@downtown65.events>`,
-  })
-
+  // const { plain, html } = getWeeklyEmail({
+  //   preferencesUrl: 'http/sdfsdsfd',
+  //   events: [
+  //     {
+  //       subtitle: 'asdads',
+  //       title: 'asdads',
+  //       date: '11.11.2022',
+  //       weekDay: '12',
+  //       participantCount: 12,
+  //       url: 'asd',
+  //       type: 'kokoasd',
+  //       typeHeader: 'Type header tässä',
+  //     },
+  //   ],
+  // })
+  // const result = await sendEmail({
+  //   subject: 'Viikon tapahtumat',
+  //   body: {
+  //     html,
+  //     text: plain,
+  //   },
+  //   to: 'pehtoorismies@gmail.com',
+  //   from: `Kyttaki <kyttaki@downtown65.events>`,
+  // })
   // const users = await fetchWeeklyEmailSubscribers()
   // console.log(users)
-
-  return result
+  // return result
 }
