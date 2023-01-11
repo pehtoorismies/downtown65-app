6ca6ca1796aa2bf70c7e109e1c6583502f091c58
pehtoorismies <pehtorismies@gmail.com>
2023-01-11 14:56:59 +0200

    tests: force click

--- packages/frontend/cypress/e2e/logged-out.cy.ts
+++ packages/frontend/cypress/e2e/logged-out.cy.ts
@@ -2,22 +2,26 @@ describe('Logged out user', () => {
   it('navigation', () => {
     cy.visit('/login')
     cy.get('h1').contains('Kirjaudu')
-    cy.get('[data-cy="to-signup"]').click()
+
+    // cypress can't click element if not forced
+    // some hydration issue?
+    // see also ./support/e2e.ts error handling <= remix mantine combo related issue perhaps
+    cy.get('[data-cy="to-signup"]').click({ force: true })
 
     cy.get('h1').contains('Rekisteröidy')
-    cy.get('[data-cy="to-login"]').click()
+    cy.get('[data-cy="to-login"]').click({ force: true })
 
     cy.get('h1').contains('Kirjaudu')
-    cy.get('[data-cy="to-forgot-password"]').click()
+    cy.get('[data-cy="to-forgot-password"]').click({ force: true })
 
     cy.get('h1').contains('Salasana unohtunut')
-    cy.get('[data-cy="to-login"]').click()
+    cy.get('[data-cy="to-login"]').click({ force: true })
 
     cy.get('h1').contains('Kirjaudu')
-    cy.get('[data-cy="button-to-signup"]').click()
+    cy.get('[data-cy="button-to-signup"]').click({ force: true })
 
     cy.get('h1').contains('Rekisteröidy')
-    cy.get('[data-cy="button-to-login"]').click()
+    cy.get('[data-cy="button-to-login"]').click({ force: true })
 
     cy.get('h1').contains('Kirjaudu')
   })
@@ -26,7 +30,7 @@ describe('Logged out user', () => {
     cy.visit('/login')
     cy.get('input[name=email]').type('someone@example.com')
     cy.get('input[name=password]').type('somepassword')
-    cy.get('form').contains('Kirjaudu').click()
+    cy.get('form').contains('Kirjaudu').click({ force: true })
     cy.contains('Virhe kirjautumisessa')
   })
 
