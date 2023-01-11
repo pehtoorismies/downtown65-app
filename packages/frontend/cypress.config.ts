6a92fbcfe453e9ad2982a47ecc3f08e970d95cda
pehtoorismies <pehtorismies@gmail.com>
2023-01-11 11:22:05 +0200

    tests: configure retries for cypress

--- packages/frontend/cypress.config.ts
+++ packages/frontend/cypress.config.ts
@@ -8,4 +8,12 @@ export default defineConfig({
     //   // implement node event listeners here
     // },
   },
+  retries: {
+    // Configure retry attempts for `cypress run`
+    // Default is 0
+    runMode: 2,
+    // Configure retry attempts for `cypress open`
+    // Default is 0
+    openMode: 0,
+  },
 })
