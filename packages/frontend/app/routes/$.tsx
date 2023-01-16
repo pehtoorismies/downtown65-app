edc73bc2fee5d591855158c4f527832d2962f0e4
pehtoorismies <pehtorismies@gmail.com>
2023-01-16 17:30:06 +0200

    tests: access test and not found page

--- packages/frontend/app/routes/$.tsx
+++ packages/frontend/app/routes/$.tsx
@@ -90,6 +90,7 @@ const NotFound = () => {
             mt="xl"
             className={classes.control}
             leftIcon={<IconArrowNarrowLeft size={18} />}
+            data-cy="navigate-home"
           >
             Etusivulle
           </Button>
