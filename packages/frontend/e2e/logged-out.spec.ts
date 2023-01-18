1dac0662bb0c1b90db58c7e40e7794df515644f6
pehtoorismies <pehtorismies@gmail.com>
2023-01-18 16:50:54 +0200

    tests: add playwright

---
+++ packages/frontend/e2e/logged-out.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('Logged out uses', () => {
+  test('has title', async ({ page }) => {
+    await page.goto('/')
+    await expect(page.getByRole('heading', { name: 'Kirjaudu' })).toBeVisible()
+    await expect(page).toHaveTitle('Dt65 - login')
+
+    await page.getByTestId('to-signup').click()
+    await expect(
+      page.getByRole('heading', { name: 'Rekisteröidy' })
+    ).toBeVisible()
+
+    await page.getByTestId('to-login').click()
+    await expect(page.getByRole('heading', { name: 'Kirjaudu' })).toBeVisible()
+
+    await page.getByTestId('to-forgot-password').click()
+    await expect(
+      page.getByRole('heading', { name: 'Salasana unohtunut' })
+    ).toBeVisible()
+
+    await page.getByTestId('to-login').click()
+    await expect(page.getByRole('heading', { name: 'Kirjaudu' })).toBeVisible()
+
+    await page.getByTestId('button-to-signup').click()
+    await expect(
+      page.getByRole('heading', { name: 'Rekisteröidy' })
+    ).toBeVisible()
+
+    await page.getByTestId('button-to-login').click()
+    await expect(page.getByRole('heading', { name: 'Kirjaudu' })).toBeVisible()
+  })
+})
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+//
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+//
+//   // Expects the URL to contain intro.
+//   await expect(page).toHaveURL(/.*intro/);
+// });
