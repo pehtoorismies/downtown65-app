974d3336607f8280623d62955b5401a67864e869
pehtoorismies <pehtorismies@gmail.com>
2023-01-25 19:05:29 +0200

    feat: remove cypress

--- packages/frontend/cypress/e2e/members.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('Members', () => {
-  it('should have some members', () => {
-    cy.loginWithDefaultUser()
-    cy.visit('/events')
-    cy.get('header').contains('Jäsenet').click({ force: true })
-    cy.contains('Seuran jäsenet')
-    cy.contains('Jäseniä yhteensä')
-  })
-})
