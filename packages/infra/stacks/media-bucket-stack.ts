8bb155a2541f94ea953b802b2fd08811fc1d0d91
pehtoorismies <pehtorismies@gmail.com>
2023-02-08 10:09:30 +0200

    infra: add s3 media bucket

---
+++ packages/infra/stacks/media-bucket-stack.ts
@@ -0,0 +1,25 @@
+import type { StackContext } from '@serverless-stack/resources'
+import { Bucket } from '@serverless-stack/resources'
+import { RemovalPolicy } from 'aws-cdk-lib'
+
+export const MediaBucketStack = ({ app, stack }: StackContext) => {
+  const removalPolicy =
+    app.stage === 'production' ? RemovalPolicy.RETAIN : RemovalPolicy.DESTROY
+
+  const bucket = new Bucket(stack, 'mediaBucket', {
+    cdk: {
+      bucket: {
+        autoDeleteObjects: true,
+        removalPolicy,
+      },
+    },
+  })
+
+  stack.addOutputs({
+    mediaBucketName: bucket.bucketName,
+  })
+
+  return {
+    mediaBucketName: bucket.bucketName,
+  }
+}
