7b303d99e562b13cae332ba5f921826d3659c1e4
pehtoorismies <pehtorismies@gmail.com>
2023-03-13 11:39:20 +0200

    feat: add profile page breadcrumbs test

--- packages/frontend/e2e/profile.spec.ts
+++ packages/frontend/e2e/profile.spec.ts
@@ -6,5 +6,21 @@ test.describe('Profile page', () => {
     await page.goto('/profile')
     await expect(page.getByTestId('profile-nick')).toHaveText(testUser.nick)
     await expect(page.getByTestId('profile-email')).toHaveText(testUser.email)
+    await expect(page.getByTestId('breadcrumbs-current')).toHaveText(
+      'Oma profiili'
+    )
+
+    await page.getByTestId('change-avatar-btn').click()
+    await expect(
+      page.getByRole('heading', { name: 'Vaihda profiilikuva' })
+    ).toBeVisible()
+    await expect(page.getByTestId('breadcrumbs-current')).toHaveText(
+      'Vaihda profiilikuva'
+    )
+    const parentPageLink = page.getByTestId('breadcrumbs-parent')
+    await expect(parentPageLink).toHaveText('Oma profiili')
+    await parentPageLink.click()
+
+    await expect(page.getByTestId('profile-nick')).toHaveText(testUser.nick)
   })
 })
