84ef6ac8742b6cb875fc47e0d403c7f7141a146b
pehtoorismies <pehtorismies@gmail.com>
2023-03-13 11:53:16 +0200

    feat: add members page breadcrumbs test

--- packages/frontend/e2e/members.spec.ts
+++ packages/frontend/e2e/members.spec.ts
@@ -5,14 +5,13 @@ test.describe('Members page', () => {
   test('should navigate to members', async ({ page }) => {
     await page.goto('/events')
     await page.getByRole('link', { name: 'Jäsenet' }).click()
-    await expect(
-      page.getByRole('heading', { name: 'Seuran jäsenet' })
-    ).toBeVisible()
+    await expect(page.getByRole('heading', { name: 'Jäsenet' })).toBeVisible()
     await expect(page.getByText(/^Jäseniä yhteensä: \d+$/)).toBeVisible()
   })
 
   test('should navigate to member profile', async ({ page }) => {
     await page.goto('/members')
+    await expect(page.getByRole('heading', { name: 'Jäsenet' })).toBeVisible()
     const memberNick = await page.getByTestId('member-nick').first()
     const memberName = await page.getByTestId('member-name').first()
     const nickname = await memberNick.textContent()
@@ -38,8 +37,22 @@ test.describe('Members page', () => {
 
     await page.getByTestId('to-members-link').click()
 
-    await expect(
-      page.getByRole('heading', { name: 'Seuran jäsenet' })
-    ).toBeVisible()
+    await expect(page.getByRole('heading', { name: 'Jäsenet' })).toBeVisible()
+  })
+
+  test('should use breadcrumbs to navigate', async ({ page }) => {
+    await page.goto('/members')
+    await expect(page.getByTestId('breadcrumbs-current')).toHaveText('Jäsenet')
+    const memberNick = await page.getByTestId('member-nick').first()
+    const nickname = await memberNick.textContent()
+    await expect(nickname).toBeDefined()
+    invariant(nickname, 'Fail')
+    await memberNick.click()
+
+    await expect(page.getByTestId('breadcrumbs-current')).toHaveText(nickname)
+    const parentPage = page.getByTestId('breadcrumbs-parent')
+    await expect(parentPage).toHaveText('Jäsenet')
+    await parentPage.click()
+    await expect(page.getByRole('heading', { name: 'Jäsenet' })).toBeVisible()
   })
 })
