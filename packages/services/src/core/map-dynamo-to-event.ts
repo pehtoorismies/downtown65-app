8cb0c5f03fc643dbfaa2313441bd36117b708879
pehtoorismies <pehtorismies@gmail.com>
2023-03-25 21:02:58 +0200

    feat: use Schema in core Event

--- packages/services/src/core/map-dynamo-to-event.ts
+++ packages/services/src/core/map-dynamo-to-event.ts
@@ -1,37 +1,8 @@
-import { z } from 'zod'
-import type { Event, EventType } from '~/appsync.gen'
-
-const PersistedDynamoEvent = z.object({
-  created: z.string(),
-  createdBy: z.object({
-    nickname: z.string(),
-    id: z.string(),
-    picture: z.string(),
-  }),
-  dateStart: z.string(),
-  description: z.string().optional(),
-  entity: z.literal('Dt65Event'),
-  id: z.string(),
-  location: z.string(),
-  modified: z.string(),
-  participants: z.record(
-    z.object({
-      nickname: z.string(),
-      joinedAt: z.string(), // validate date
-      id: z.string(),
-      picture: z.string(),
-    })
-  ),
-  race: z.boolean(),
-  subtitle: z.string(),
-  timeStart: z.string().optional(),
-  title: z.string(),
-  type: z.string(), // should be enum
-  updated: z.string().optional(),
-})
+import type { Event } from '~/appsync.gen'
+import { Dt65EventGetSchema } from '~/core/dynamo-schemas/dt65-event-schema'
 
 export const mapDynamoToEvent = (persistedDynamoItem: unknown): Event => {
-  const result = PersistedDynamoEvent.safeParse(persistedDynamoItem)
+  const result = Dt65EventGetSchema.safeParse(persistedDynamoItem)
 
   if (!result.success) {
     throw new Error(
@@ -43,8 +14,6 @@ export const mapDynamoToEvent = (persistedDynamoItem: unknown): Event => {
   const parsed = result.data
   return {
     ...parsed,
-    type: parsed.type as EventType, // should check for enum
-    // eslint-disable-next-line no-unused-vars
     participants: Object.entries(parsed.participants)
       // eslint-disable-next-line no-unused-vars
       .map(([_, value]) => {
