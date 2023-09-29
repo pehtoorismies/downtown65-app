261b2ecf6519cbe7998ac71d5a50911a1ebad905
pehtoorismies <pehtorismies@gmail.com>
2023-09-29 10:54:23 +0300

    chore: allow null in package: functions

--- packages/functions/.eslintrc.cjs
+++ packages/functions/.eslintrc.cjs
@@ -2,6 +2,7 @@ const OFF = 0;
 
 module.exports = {
   rules: {
-    "unicorn/text-encoding-identifier-case": OFF
+    "unicorn/text-encoding-identifier-case": OFF,
+    "unicorn/no-null": OFF
   }
 };
