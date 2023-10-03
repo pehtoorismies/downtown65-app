20cf333be0c85bc809cc294134734007425aa9c9
pehtoorismies <pehtorismies@gmail.com>
2023-10-03 14:18:40 +0300

    chore(web): trial and error to get remix v2 to start;
    remix.config must be in esm-format and some packages have to bundled

--- packages/web/remix.config.js
+++ packages/web/remix.config.js
@@ -1,7 +1,15 @@
 /** @type {import("@remix-run/dev").AppConfig} */
-module.exports = {
-  ignoredRouteFiles: ['**/modules/**.*'],
+export default {
+  ignoredRouteFiles: ['**/.*'],
   postcss: true,
+  serverDependenciesToBundle: [
+    /^date-fns.*/,
+    /^dayjs.*/,
+    /^@mantine\/dropzone.*/,
+    'react-dropzone',
+  ],
+  // serverModuleFormat: 'esm',
+  // serverModuleFormat: "cjs",
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
