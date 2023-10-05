97802676664becf3dd8e2d3c88c0c8b03b573438
pehtoorismies <pehtorismies@gmail.com>
2023-10-05 12:49:22 +0300

    refactor(web): separate theme

--- packages/web/app/root.tsx
+++ packages/web/app/root.tsx
@@ -11,7 +11,6 @@ import {
   Menu,
   Text,
   UnstyledButton,
-  createTheme,
 } from '@mantine/core'
 import { useDisclosure } from '@mantine/hooks'
 import { Notifications, notifications } from '@mantine/notifications'
@@ -41,47 +40,7 @@ import classes from './routes-styles/root.module.css'
 import type { User } from '~/domain/user'
 import type { ToastMessage } from '~/message.server'
 import { commitMessageSession, getMessageSession } from '~/message.server'
-
-const theme = createTheme({
-  colors: {
-    dtPink: [
-      '#F7D9F2',
-      '#F6AFEA',
-      '#FF80EA', // original
-      '#EE6AD9',
-      '#DB5BC6',
-      '#C751B3',
-      '#B14AA0',
-      '#964C8A',
-      '#804B78',
-      '#804B78',
-    ],
-  },
-  defaultGradient: { from: 'indigo', to: 'cyan', deg: 45 },
-  shadows: {
-    md: '1px 1px 3px rgba(0, 0, 0, .25)',
-    xl: '5px 5px 3px rgba(0, 0, 0, .25)',
-  },
-  // components: {
-  //   Container: {
-  //     defaultProps: {
-  //       sizes: {
-  //         xs: 540,
-  //         sm: 720,
-  //         md: 960,
-  //         lg: 1140,
-  //         xl: 1320,
-  //       },
-  //     },
-  //   },
-  // },
-  headings: {
-    fontFamily: 'Roboto, sans-serif',
-    sizes: {
-      h1: { fontSize: '2rem' },
-    },
-  },
-})
+import { theme } from '~/routes-styles/theme'
 
 const ONE_YEAR = 1000 * 60 * 60 * 24 * 36
 
