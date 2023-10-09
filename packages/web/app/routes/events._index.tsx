48e4e5f62fb3b719a2cfbe579c1a5cab3a2d7d2b
pehtoorismies <pehtorismies@gmail.com>
2023-10-09 17:31:15 +0300

    feat(web): adjust events page

--- packages/web/app/routes/events._index.tsx
+++ packages/web/app/routes/events._index.tsx
@@ -12,6 +12,7 @@ import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node'
 import { json } from '@remix-run/node'
 import { Link, useLoaderData } from '@remix-run/react'
 import { IconSquarePlus } from '@tabler/icons-react'
+import type { ReactNode } from 'react'
 import React from 'react'
 import { EventCard } from '~/components/event-card/event-card'
 import {
@@ -69,50 +70,53 @@ const EventsBreadcrumb = () => {
   )
 }
 
+const Root = ({ children }: { children: ReactNode }) => (
+  <>
+    <EventsBreadcrumb />
+    <Container data-testid="events" p="xs" size="1000">
+      {children}
+    </Container>
+  </>
+)
+
 export default function GetEvents() {
   const { eventItems } = useLoaderData<typeof loader>()
   const participationActions = useParticipationActions()
 
   if (eventItems.length === 0) {
     return (
-      <>
-        <EventsBreadcrumb />
-        <Container data-testid="events">
-          <Title order={1} ta="center">
-            Ei tulevia tapahtumia
-          </Title>
-          <Center>
-            <Button
-              component={Link}
-              to="/events/new"
-              size="lg"
-              mt="xs"
-              rightSection={<IconSquarePlus size={30} />}
-            >
-              Luo uusi tapahtuma
-            </Button>
-          </Center>
-        </Container>
-      </>
+      <Root>
+        <Title order={1} ta="center">
+          Ei tulevia tapahtumia
+        </Title>
+        <Center>
+          <Button
+            component={Link}
+            to="/events/new"
+            size="lg"
+            mt="xs"
+            rightSection={<IconSquarePlus size={30} />}
+          >
+            Luo uusi tapahtuma
+          </Button>
+        </Center>
+      </Root>
     )
   }
 
   return (
-    <>
-      <EventsBreadcrumb />
-      <Container data-testid="events">
-        <ParticipatingContext.Provider value={participationActions}>
-          <SimpleGrid
-            cols={{ base: 1, sm: 2 }}
-            spacing={{ base: 10, sm: 'md', md: 'xl' }}
-            verticalSpacing={{ base: 'md', sm: 'xl' }}
-          >
-            {eventItems.map((m) => {
-              return <EventCard key={m.id} {...m} shadow="xs" />
-            })}
-          </SimpleGrid>
-        </ParticipatingContext.Provider>
-      </Container>
-    </>
+    <Root>
+      <ParticipatingContext.Provider value={participationActions}>
+        <SimpleGrid
+          cols={{ base: 1, sm: 2 }}
+          spacing={{ base: 'sm', md: 'xl' }}
+          verticalSpacing={{ base: 'sm', md: 'xl' }}
+        >
+          {eventItems.map((m) => {
+            return <EventCard key={m.id} {...m} shadow="xs" />
+          })}
+        </SimpleGrid>
+      </ParticipatingContext.Provider>
+    </Root>
   )
 }
