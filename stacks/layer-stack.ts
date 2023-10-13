22045ac4bf54f3d0bbae156c5983665428c10b11
pehtoorismies <pehtorismies@gmail.com>
2023-10-13 11:21:35 +0300

    infra: use sharp as lambda layer inside remix;
    This is a result of a trial and error...

---
+++ stacks/layer-stack.ts
@@ -0,0 +1,17 @@
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import { StackContext } from 'sst/constructs'
+
+export const LayerStack = ({ stack }: StackContext) => {
+  const sharpLayer = new lambda.LayerVersion(stack, 'AppLayer', {
+    code: lambda.Code.fromAsset('stacks/layers/sharp'),
+    // compatibleArchitectures: [lambda.Architecture.ARM_64],
+  })
+
+  stack.addOutputs({
+    SHARP_LAYER_ARN: sharpLayer.layerVersionArn,
+  })
+
+  return {
+    SHARP_LAYER: sharpLayer,
+  }
+}
