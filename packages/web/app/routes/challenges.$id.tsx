206d6b825ea18a603ef8b6ceb30cbaa1132c8454
pehtoorismies <pehtorismies@gmail.com>
2023-11-05 20:34:08 +0200

    feat(challenge): improve page

--- packages/web/app/routes/challenges.$id.tsx
+++ packages/web/app/routes/challenges.$id.tsx
@@ -3,17 +3,18 @@ import {
   Button,
   Center,
   Container,
-  Divider,
   Group,
   Paper,
   Stack,
   Text,
+  Title,
+  TypographyStylesProvider,
 } from '@mantine/core'
 import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node'
 import { json } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
 import { IconCheck, IconCircle } from '@tabler/icons-react'
-import { formatISO } from 'date-fns'
+import { differenceInCalendarDays, format, formatISO, parseISO } from 'date-fns'
 import React from 'react'
 import { LeaderboardRow } from '~/components/challenge/leaderboard-row'
 import { ToggleJoinButton } from '~/components/event-card/toggle-join-button'
@@ -22,6 +23,7 @@ import type { Context } from '~/contexts/participating-context'
 import { ParticipatingContext } from '~/contexts/participating-context'
 import type { ChallengeParticipant } from '~/domain/user'
 import { loaderAuthenticate } from '~/session.server'
+import { addChallengePosition } from '~/util/add-challenge-position'
 import { getChallengeDates } from '~/util/challenge-date'
 import { logger } from '~/util/logger.server'
 
@@ -34,30 +36,52 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
       doneDatesCount: 6,
     },
     {
-      id: '12',
+      id: '10',
       nickname: 'Koira',
       picture: 'https://picsum.photos/100/100',
       doneDatesCount: 5,
     },
     {
-      id: '11',
+      id: '20',
       nickname: 'Kissa',
-      picture: 'https://picsum.photos/100',
+      picture: 'https://picsum.photos/100?123',
       doneDatesCount: 2,
+    },
+    {
+      id: '30',
+      nickname: 'Hevoinen',
+      picture: 'https://picsum.photos/100?12',
+      doneDatesCount: 2,
+    },
+    {
+      id: '40',
+      nickname: 'Hevoinen',
+      picture: 'https://picsum.photos/100?12',
+      doneDatesCount: 8,
     },
   ]
 
   const myDoneDates = ['2023-11-01', '2023-11-02', '2023-11-03']
 
   const challenge = {
-    title: 'Kyykkyhaaste - Lokakuu 2023',
+    title: 'Kyykkyhaaste - Marraskuu 2023',
     subtitle: '100 kyykkyä päivässä',
     startDate: '2023-11-01',
     endDate: '2023-11-30',
-    participants,
+    kind: 'daily',
+    participants: addChallengePosition(participants),
+    description: '<h2>Kk haaste</h2><h3>kissa</h3>',
   }
 
-  logger.debug(participants, 'Participants')
+  const endDate = parseISO(challenge.endDate)
+  const today = new Date()
+  const start = parseISO(challenge.startDate)
+  const end = parseISO(challenge.endDate)
+
+  const runningTime = `${format(start, 'd.M.yyyy')} - ${format(
+    end,
+    'd.M.yyyy'
+  )}`
 
   const dates = getChallengeDates({
     startISODate: challenge.startDate,
@@ -71,8 +95,10 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 
   return json({
     user,
-    buttons: dates,
+    doneDates: dates,
     today: 'KOIRA',
+    runningTime,
+    daysLeft: differenceInCalendarDays(endDate, today),
     challenge: {
       ...challenge,
       daysCount: dates.length,
@@ -89,7 +115,12 @@ export const meta: MetaFunction = () => {
 }
 
 export default function GetChallenge() {
-  const { user, challenge, buttons, today } = useLoaderData<typeof loader>()
+  const { user, challenge, doneDates, runningTime, daysLeft, today } =
+    useLoaderData<typeof loader>()
+
+  const dayFi = daysLeft === 1 ? 'päivä' : 'päivää'
+
+  const dayInfo = `${daysLeft} ${dayFi} jäljellä`
 
   const participationActions: Context = {
     onParticipate: (eventId: string) => {},
@@ -98,12 +129,13 @@ export default function GetChallenge() {
     loadingEventId: undefined,
   }
 
-  const runningTime = `${challenge.startDate} - ${challenge.endDate}}`
   const isParticipating =
     user !== null &&
     challenge.participants.map(({ id }) => id).includes(user.id)
 
-  const buttonList = buttons.map(({ status, text }) => {
+  const hasDoneChallenges = doneDates.length > 0
+
+  const buttonList = doneDates.map(({ status, text }) => {
     switch (status) {
       case 'DONE': {
         return (
@@ -123,8 +155,8 @@ export default function GetChallenge() {
           <Button
             key={text}
             style={{ width: 150 }}
-            variant="default"
-            color="green"
+            variant="outline"
+            color="red"
             leftSection={<IconCircle size={14} />}
           >
             {text}
@@ -167,33 +199,51 @@ export default function GetChallenge() {
                 100 kyykkyä päivässä
               </Text>
               <Text size="sm" fw={500}>
-                {runningTime}
+                Omaan tahtiin
               </Text>
               <Text size="sm" c="dimmed" fw={400}>
-                kk-haaste
+                {runningTime}
+              </Text>
+              <Text size="sm" fw={500}>
+                {dayInfo}
               </Text>
             </Box>
             <Group>
               {isParticipating && (
-                <Button>Merkitse päivä tehdyksi ({today})</Button>
+                <Button leftSection={<IconCircle size={14} />}>
+                  Merkitse tämä päivä tehdyksi
+                </Button>
               )}
-              <ParticipatingContext.Provider value={participationActions}>
-                <ToggleJoinButton
-                  isParticipating={isParticipating}
-                  eventId="someUd"
-                />
-              </ParticipatingContext.Provider>
+              {!hasDoneChallenges && (
+                <ParticipatingContext.Provider value={participationActions}>
+                  <ToggleJoinButton
+                    isParticipating={isParticipating}
+                    eventId="someUd"
+                  />
+                </ParticipatingContext.Provider>
+              )}
             </Group>
           </Group>
-          <Divider label="Leaderboard" my="sm" labelPosition="center" />
+          <Paper bg="#FAFAF8" my="sm">
+            <TypographyStylesProvider p="xs" m={0}>
+              <div
+                dangerouslySetInnerHTML={{ __html: challenge.description }}
+              />
+            </TypographyStylesProvider>
+          </Paper>
           <Center>
-            <Paper shadow="sm" p="xs" withBorder>
+            <Title order={2} my="md">
+              Leaderboard
+            </Title>
+          </Center>
+          <Center>
+            <Paper shadow="sm" p="xs" withBorder style={{ width: '100%' }}>
               <Stack my="md">
                 {challenge.participants.map((cp) => {
                   return (
                     <LeaderboardRow
                       key={cp.id}
-                      position={1}
+                      position={cp.position}
                       participant={cp}
                       daysTotal={challenge.daysCount}
                       daysDone={cp.doneDatesCount}
@@ -203,11 +253,17 @@ export default function GetChallenge() {
               </Stack>
             </Paper>
           </Center>
-          <Divider label="Omat suoritukset" my="sm" labelPosition="center" />
-          <Group m="xs" gap="xs" justify="center">
+          <Center>
+            <Title my="md" order={2}>
+              Omat suoritukset
+            </Title>
+          </Center>
+          <Center>
+            <Text>Muokkaa menneitä suorituksia klikkaamalla päivää.</Text>
+          </Center>
+          <Group m="sm" gap="xs" justify="center">
             {buttonList}
           </Group>
-          <Divider />
         </Voucher.Content>
       </Voucher>
     </Container>
