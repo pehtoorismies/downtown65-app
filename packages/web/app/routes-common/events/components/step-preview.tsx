ca9c66bac4e59a859c6780d47e7bb14884f0793f
pehtoorismies <pehtorismies@gmail.com>
2023-12-21 12:20:31 +0200

    refactor: reorganize step-preview

--- packages/web/app/routes-common/events/components/step-preview.tsx
+++ packages/web/app/routes-common/events/components/step-preview.tsx
@@ -1,15 +1,22 @@
+import { IconDeviceFloppy, IconRocket } from '@tabler/icons-react'
 import { format } from 'date-fns'
 import { fi } from 'date-fns/locale'
 import React from 'react'
 import type { EventState } from './event-state'
+import { Gradient } from '~/components/colors'
 import { EventCard } from '~/components/event/event-card'
 import type { User } from '~/domain/user'
-import { Heading } from '~/routes-common/events/components/heading'
+import {
+  NextButton,
+  PrevButton,
+  StepLayout,
+} from '~/routes-common/events/components/buttons'
+import type { ReducerProps } from '~/routes-common/events/components/reducer'
 import { prefixZero, suffixZero } from '~/util/pad-zeros'
 
-interface Properties {
-  state: EventState
+interface Properties extends ReducerProps {
   me: User
+  submit: () => void
 }
 
 const getDate = (date: EventState['date']) => {
@@ -25,14 +32,49 @@ const getTime = ({ hours, minutes }: EventState['time']) => {
   }
 }
 
-export const StepPreview = ({ state, me }: Properties) => {
+const getButtonProps = (
+  kind: EventState['kind']
+): { text: string; icon: React.ReactNode } => {
+  switch (kind) {
+    case 'create': {
+      return { text: 'Luo tapahtuma', icon: <IconRocket size={18} /> }
+    }
+    case 'edit': {
+      return { text: 'Tallenna', icon: <IconDeviceFloppy size={18} /> }
+    }
+  }
+}
+
+export const StepPreview = ({ state, me, dispatch, submit }: Properties) => {
   if (!state.eventType) {
     throw new Error('Illegal state, not eventType defined')
   }
 
+  const previousButton = (
+    <PrevButton onClick={() => dispatch({ kind: 'previousStep' })}>
+      Kuvaus
+    </PrevButton>
+  )
+
+  const { text, icon } = getButtonProps(state.kind)
+
+  const nextButton = (
+    <NextButton
+      onClick={submit}
+      rightSection={icon}
+      gradient={Gradient.dtPink}
+      variant="gradient"
+    >
+      {text}
+    </NextButton>
+  )
+
   return (
-    <>
-      <Heading>Esikatselu</Heading>
+    <StepLayout
+      title="Esikatselu"
+      prevButton={previousButton}
+      nextButton={nextButton}
+    >
       <EventCard
         title={state.title}
         race={state.isRace}
@@ -44,6 +86,6 @@ export const StepPreview = ({ state, me }: Properties) => {
         dateStart={getDate(state.date)}
         timeStart={getTime(state.time)}
       />
-    </>
+    </StepLayout>
   )
 }
