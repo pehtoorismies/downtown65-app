095ae9c09410c4a403395cf5832d1b60f8a251dc
pehtoorismies <pehtorismies@gmail.com>
2023-12-21 13:34:01 +0200

    chore(remix): comment SST config

--- packages/web/remix.config.js
+++ packages/web/remix.config.js
@@ -1,5 +1,6 @@
 /** @type {import("@remix-run/dev").AppConfig} */
 export default {
+  // check ignoredRouteFiles https://docs.sst.dev/constructs/RemixSite
   ignoredRouteFiles: ['**/.*'],
   postcss: true,
   serverDependenciesToBundle: [/^date-fns.*/, /^dayjs.*/],
