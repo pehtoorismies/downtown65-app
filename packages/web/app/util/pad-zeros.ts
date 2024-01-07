aa785689b5f0f1d6464ac23cea89980451aea5a7
pehtoorismies <pehtorismies@gmail.com>
2024-01-07 19:17:10 +0200

    refactor: remove unused file

--- packages/web/app/util/pad-zeros.ts
+++
@@ -1,6 +0,0 @@
-export const prefixZero = (value: number): string => {
-  return value < 10 ? `0${value}` : `${value}`
-}
-export const suffixZero = (value: number): string => {
-  return value < 10 ? `${value}0` : `${value}`
-}
