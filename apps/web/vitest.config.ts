7ebdaad6db6dfc6b077dd2326b6a0382daf17de7
pehtoorismies <pehtorismies@gmail.com>
2024-02-03 13:35:09 +0200

    chore: fix tests

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,12 +1,12 @@
 /// <reference types="vitest" />
 
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     testTimeout: 90_000,
-    exclude: ['e2e/**/*.ts'],
+    exclude: [...configDefaults.exclude, 'e2e/**/*.ts'],
   },
   logLevel: 'info',
   esbuild: {
