1f82596e581e151dab120ee500c0d41a2f47a05a
pehtoorismies <pehtorismies@gmail.com>
2024-02-03 17:52:06 +0200

    chore: fix root level lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,43 +1,11 @@
+const file = require.resolve("@downtown65-app/config-eslint/package.cjs");
+
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "plugin:unicorn/recommended",
-    "prettier"
-  ],
-  plugins: ["import", "unicorn", "@typescript-eslint", "unused-imports", "prettier"],
-  rules: {
-    "@typescript-eslint/consistent-type-imports": "error",
-    "import/order": [
-      "error",
-      {
-        alphabetize: {
-          order: "asc",
-          caseInsensitive: true
-        },
-        "newlines-between": "never"
-      }
-    ],
-    "prettier/prettier": ["error"],
-    "sort-imports": ["error", { ignoreDeclarationSort: true }]
-  },
-  env: {
-    node: true
-  },
-  settings: {
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"]
-    },
-    "import/resolver": {
-      typescript: true,
-      node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"]
-      }
-    }
+  extends: [file],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true
   }
 };
