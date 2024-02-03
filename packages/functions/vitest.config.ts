c32aff87f193a54d3bd63f9c638cc5752d24aeac
pehtoorismies <pehtorismies@gmail.com>
2024-02-03 13:35:09 +0200

    chore: fix tests

--- packages/functions/vitest.config.ts
+++ packages/functions/vitest.config.ts
@@ -1,7 +1,11 @@
 /// <reference types="vitest" />
 
+import dotenv from 'dotenv'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { configDefaults, defineConfig } from 'vitest/config'
+
+const envFile = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env'
+dotenv.config({ path: envFile })
 
 export default defineConfig({
   test: {
