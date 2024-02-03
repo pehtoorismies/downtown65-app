c4b784e6e203cf554fba2bc1f12d38174df5929c
pehtoorismies <pehtorismies@gmail.com>
2024-02-03 19:11:43 +0200

    fix: style via prettier

--- apps/backend/.eslintrc.cjs
+++ apps/backend/.eslintrc.cjs
@@ -1,14 +1,14 @@
-const file = require.resolve("@downtown65-app/config-eslint/package.cjs");
+const file = require.resolve('@downtown65-app/config-eslint/package.cjs')
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: [file],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: true
+    project: true,
   },
   rules: {
-    "unicorn/text-encoding-identifier-case": 0,
-    "unicorn/no-null": 0
-  }
-};
+    'unicorn/text-encoding-identifier-case': 0,
+    'unicorn/no-null': 0,
+  },
+}
