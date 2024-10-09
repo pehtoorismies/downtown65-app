34edce22afffcbb80b3548972e72bc79bb490503
pehtoorismies <pehtorismies@gmail.com>
2024-10-09 16:41:39 +0300

    feat: remove unused scopes

--- apps/backend/src/graphql-appsync/resolvers/verify-scope.ts
+++ apps/backend/src/graphql-appsync/resolvers/verify-scope.ts
@@ -12,7 +12,6 @@ interface Dt65Identity extends AppSyncIdentityOIDC {
     azp: string
     scope: string
     iss: string
-    ['https://graphql.downtown65.com/nickname']: string
     exp: number
     iat: number
     gty: string
