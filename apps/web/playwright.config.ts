f7459a260c01d695ffacd51e95e9cbfc19fcba3c
pehtoorismies <pehtorismies@gmail.com>
2025-02-07 13:07:55 +0200

    test: use playwright video

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -51,6 +51,7 @@ const config: PlaywrightTestConfig = {
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     screenshot: 'only-on-failure',
+    video: 'on-first-retry',
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
