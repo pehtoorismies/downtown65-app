73edd2940b7d1f4fe004bf121a9192ae569aae56
pehtoorismies <pehtorismies@gmail.com>
2025-03-26 14:05:24 +0200

    feat: use Config varianle for honeypot

--- apps/web/app/honeypot.server.ts
+++ apps/web/app/honeypot.server.ts
@@ -1,4 +1,5 @@
 import { Honeypot } from 'remix-utils/honeypot/server'
+import { Config } from 'sst/node/config'
 
 // Create a new Honeypot instance, the values here are the defaults, you can
 // customize them
@@ -6,5 +7,5 @@ export const honeypot = new Honeypot({
   randomizeNameFieldName: false,
   nameFieldName: 'name__confirm',
   validFromFieldName: 'from__confirm', // null to disable it
-  encryptionSeed: undefined, // Ideally it should be unique even between processes
+  encryptionSeed: Config.HONEYPOT_SECRET, // Ideally it should be unique even between processes
 })
